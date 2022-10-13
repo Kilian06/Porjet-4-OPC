0fa51306cd0f5e6eacf16cb05b5a67f859c7beba
Kilian LEROY <kilian.leroy@me.com>
2022-10-13 10:14:59 +0200

    suppression console.log

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -36,7 +36,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-  console.log("ouverture de la modal");
 }
 
 // submitBtn.addEventListener("click", verifForm);
@@ -45,8 +44,6 @@ function launchModal() {
 function validate(event){
   event.preventDefault();
   var modalHeight = document.querySelector(".content").clientHeight;
-  console.log(modalHeight);
-  console.log("Vérification du formulaire de la modal");
 
   //Défniition des Regex
 
@@ -58,7 +55,6 @@ function validate(event){
 
   if(inputFirst.value.length < 2){
     var inputFirsterr = ("Le Prenom saisie n'est pas valide.\n");
-    console.log(inputFirsterr);
     var inputFirsterrMsg = document.getElementById("prenomErr");
     inputFirsterrMsg.style.display ="block";
     let parentInputFirst = inputFirst.parentNode;
@@ -75,7 +71,6 @@ function validate(event){
 // Vérification du nom
   if(inputLast.value.length < 2){
     var inputLasterr = ("Le nom saisie n'est pas valide.\n");
-    console.log(inputLasterr)
     let inputLasterrMsg = document.getElementById('nomErr');
     inputLasterrMsg.style.display ="block";
     let parentInputLast = inputLast.parentNode;
@@ -102,7 +97,6 @@ function validate(event){
   else
   {
     var inputEmailErr = "L'email saisi n'est pas valide\n"
-    console.log(inputEmailErr);
     let inputMailerrMsg = document.getElementById('emailErr');
     inputMailerrMsg.style.display ="block";
     let parentInputEmail = inputEmail.parentNode;
@@ -120,8 +114,6 @@ function validate(event){
     else
     {
       var inputDateErr = "La date saisie n'est pas valide.\n"
-      console.log(inputDateErr)
-      console.log(inputBirthdate.value);
       let inputDateerrMsg = document.getElementById('birthdateErr');
       inputDateerrMsg.style.display ="block";
       let parentInputBirthdate = inputBirthdate.parentNode;
@@ -142,7 +134,6 @@ function validate(event){
       else
       {
         var inputQuantityErr = "La quantité saisie n'est pas valide\n"
-        console.log(inputQuantityErr);
         let inputQuantityErrMsg = document.getElementById('quantityErr');
         inputQuantityErrMsg.style.display ="block";
         let parentInputQuantity = inputQuantity.parentNode;
@@ -169,7 +160,6 @@ function validate(event){
       inputCheckbox1ErrMsg.style.display ="none";
     }else{
       var inputCheckbox1Err = "Veuillez accepter les conditions d'utilisation";
-      console.log(inputCheckbox1Err);
       let inputCheckbox1ErrMsg = document.getElementById('checkbox1Err');
       inputCheckbox1ErrMsg.style.display ="block";
     };
@@ -178,14 +168,12 @@ function validate(event){
     if(inputFirsterr + inputLasterr +inputEmailErr + inputDateErr + inputQuantityErr + inputLocationErr + inputCheckbox1Err === "")
     {
       
-      console.log(modalHeight);
       viewContentSucess.style.height = modalHeight+"px";
       contentForm.style.display="none";
       viewContentSucess.style.display="block";
       boxContentSucess.style.height = modalHeight - 100 +"px";
       msgContentSucess.style.paddingTop = (modalHeight / 2) - 50 + "px";
       document.getElementById('formid').reset();
-      console.log("normalement j'affiche que c'est bien recu");
     }
     else{
       console.log(inputFirsterr + inputLasterr +inputEmailErr + inputDateErr + inputQuantityErr + inputLocationErr + inputCheckbox1Err);
@@ -194,19 +182,16 @@ function validate(event){
 
 // Fermeture de la modal
 closeConst.addEventListener('click', event => {
-  console.log("ok")
   modalbg.style.display="none"
 })
 
 closeConst2.addEventListener('click', event => {
-  console.log("ok")
   modalbg.style.display="none";
   contentForm.style.display="block";
   viewContentSucess.style.display="none";
 })
 
 function closecontentsecc(){
-  console.log("Fermeture de la popin de confirmation");
   modalbg.style.display="none";
   contentForm.style.display="block";
   viewContentSucess.style.display="none";
